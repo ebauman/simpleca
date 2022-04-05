fdcda820ad0971e3e877085f4bf8e6506b8679b9
Julio Hawthorne <julio@vltraheaven.io>
2022-04-04 21:16:50 -0700

    Interactive prompt `cert sign` functionality fully implemented

---
+++ cmd/interactive/cert/sign.go
@@ -0,0 +1,100 @@
+package cert
+
+import (
+	"github.com/ebauman/simpleca/tls"
+	"github.com/manifoldco/promptui"
+	"net"
+	"reflect"
+	"strings"
+)
+
+func signUI() (err error) {
+	var prompts = map[string]promptui.Prompt{
+		"Passphrase": promptui.Prompt{
+			Label:       "Passphrase (default: changeme)",
+			Mask:        '*',
+			HideEntered: true,
+			Default:     "changeme",
+		},
+		"Country": promptui.Prompt{
+			Label:   "Country code",
+			Default: "AA",
+		},
+		"State": promptui.Prompt{
+			Label:   "State",
+			Default: "Relaxation",
+		},
+		"Locality": promptui.Prompt{
+			Label:   "Locality",
+			Default: "",
+		},
+		"Organization": promptui.Prompt{
+			Label:   "Organization",
+			Default: "SimpleCA Ltd.",
+		},
+		"OrganizationalUnit": promptui.Prompt{
+			Label:   "Organizational unit",
+			Default: "SimpleCA Security",
+		},
+		"IPAddresses": promptui.Prompt{
+			Label: "Comma-delimited list of IP Addresses",
+		},
+		"DNSNames": promptui.Prompt{
+			Label: "Comma-delimited list of DNS Names",
+		},
+		"EmailAddresses": promptui.Prompt{
+			Label: "Comma-delimited list of Email Addresses",
+		},
+		"URIs": promptui.Prompt{
+			Label: "Comma-delimited list of URI Subject Names",
+		},
+		"ExpireIn": promptui.Prompt{
+			Label:   "Duration of cert validity",
+			Default: "1 year",
+		},
+		"Name": promptui.Prompt{
+			Label:   "Certificate Name",
+			Default: "default-cert",
+		},
+		"Path": promptui.Prompt{
+			Label:   "Path to CA store directory",
+			Default: caPath,
+		},
+		"CommonName": promptui.Prompt{
+			Label: "Common Name",
+		},
+	}
+	fields := reflect.TypeOf(tls.CertConfig{})
+
+	for i := 0; i < fields.NumField(); i++ {
+		fieldName := fields.Field(i).Name
+		prompt := prompts[fieldName]
+		res, err := prompt.Run()
+		if err != nil {
+			return err
+		}
+
+		slicer := func(s string) (slice []string) {
+			for _, sl := range strings.Split(s, ",") {
+				slice = append(slice, sl)
+			}
+			return
+		}
+
+		switch strings.ToLower(fieldName) {
+		case "ipaddresses":
+			var netIP []net.IP
+			for _, ip := range strings.Split(res, ",") {
+				netIP = append(netIP, net.ParseIP(ip))
+			}
+			reflect.ValueOf(certConfig).Elem().FieldByName(fieldName).Set(
+				reflect.ValueOf(netIP))
+		case "dnsnames", "emailaddresses", "uris":
+			reflect.ValueOf(certConfig).Elem().FieldByName(fieldName).Set(
+				reflect.ValueOf(slicer(res)))
+		default:
+			reflect.ValueOf(certConfig).Elem().FieldByName(fieldName).SetString(res)
+		}
+	}
+	return tls.SignCert(certConfig, caName)
+}
--- cmd/interactive/interactive.go
+++ cmd/interactive/interactive.go
@@ -1,11 +1,11 @@
 package interactive
 
 import (
-	"fmt"
 	"github.com/ebauman/simpleca/cmd/interactive/ca"
 	"github.com/ebauman/simpleca/cmd/interactive/cert"
 	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
+	"log"
 )
 
 // Interactivecmd allows user-input via interactive terminal prompts
@@ -15,12 +15,13 @@ var Interactivecmd = &cobra.Command{
 	Long:  `Manage CA and Certificates using an interactive prompt.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		prompt := promptui.Select{
-			Label: "Choose management target",
+			Label: "Choose management prompt",
 			Items: []string{"ca", "cert"},
 		}
 		_, res, err := prompt.Run()
 		if err != nil {
-			fmt.Errorf("an error has occured: %w", err)
+			log.Println("an error has occured: %w", err)
+			return
 		}
 		switch res {
 		case "ca":
